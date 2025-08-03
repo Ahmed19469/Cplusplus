35466eebe731b22821849b1213b60f8fd97bce5f
Ahmed19469 <Ahmedmarash2006@gmail.com>
2025-08-03 23:56:42 +0930

    added code for some medium problems 2-1 and 2-2

---
+++ function-2-1.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+
+int min_element(int array[], int n)
+{
+  int min =array[0];
+if (n<1)
+{
+return 0;
+}
+else{
+    for (int i=1;i<n;i++)
+    {
+
+        if(array[i]<min)
+        {
+            min =array[i];
+
+        }
+
+        return min;
+    }
+}
+}
---
+++ function-2-2.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+
+int max_element(int array[], int n)
+{
+  int max =array[0];
+if (n<1)
+{
+return 0;
+}
+else{
+    for (int i=1;i<n;i++)
+    {
+
+        if(array[i]>max)
+        {
+            max =array[i];
+
+        }
+
+        return max;
+    }
+}
+}
