83a8106bf2ba339dead7fdb1027c9117245e4bf1
Ahmed19469 <Ahmedmarash2006@gmail.com>
2025-08-04 00:23:26 +0930

    all medium problems uploaded

---
+++ function-2-5.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+bool is_descending(int array[], int n){
+int flag=0;
+    for(int i =0;i<n-1;i++)
+    {
+        if(array[i+1]<array[i]){
+            flag=1;
+        }
+    }
+    if(flag==0 && n>0)
+    {
+        return true;
+    }
+    else
+    {return false;}
+}
